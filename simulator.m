acfe49048b5b677ab7434822f5344efbea6f03bc
jhmitchel <jhmitchel@gmail.com>
2017-04-04 11:17:05 -0400

    added logic for intersection (without intersections)

--- simulator.m
+++ simulator.m
@@ -9,6 +9,7 @@ speed_limit = 40; % mph
 time_step = (car_length / 63360) * (3600 / speed_limit); % seconds (car lengthing in miles / miles per second)
 tot_time = 30 * 60; % seconds (minutes * minutes/second)
 num_lights =  10; % NOT ACCURATE - for allocation
+max_queue = 25;
 
 % Washtenaw - rows are distance, columns are lanes
 % 0s are empty spots, greater than 0s is car index.
@@ -32,11 +33,16 @@ traffic_map = zeros(num_spots, 1);
 % cols are (more can be added)
 %   (1) 0 for green down Washtenaw and red for turning,
 %       1 for red down Washtenaw and green for turning
-light_table = zeros(num_lights, 1);
+%   (2) index of queue turning right onto Washtenaw at intersection
+%   (3) index of queue turning left onto Washtenaw at intersection
+light_table = zeros(num_lights, 3);
 
 % TODO: create lights at approx real intersections
 
-% TODO: create queues for each light 
+% create two queues for each light - 1 for left turns and 1 for right turns
+% cells in a row are the car index, queue order left to right
+queue_table = zeros(2 * num_lights, max_queue);
+
 
 % start simulation
 for t = 0:time_step:max_time
@@ -58,5 +64,51 @@ for t = 0:time_step:max_time
 %     end
     
     % TODO: check intersection turns
+    for cur_spot = 1:num_spots
+        light_index = traffic_map(cur_spot);
+        
+        if light_index > 0
+            green_light = light_table(light_index, 1);
+            left_queue = light_table(light_index, 2);
+            right_queue = light_table(light_index, 3);
+            
+            % turn logic for green light
+            if green_light
+                % left turn queue
+                left_car = queue_table(left_queue, 1);
+                if left_car > 0
+                    spot_empty = (washtenaw(cur_spot, 1) == 0);
+                    if spot_empty
+                        assert(next_washtenaw(cur_spot, 2) == 0);
+                        next_washtenaw(cur_spot, 1) = left_car;
+                        % TODO: shift queue
+                    end
+                end
+                
+                % right turn queue
+                right_car = queue_table(right_queue, 2);
+                if right_car > 0
+                    spot_empty = (washtenaw(cur_spot, 2) == 0);
+                    if spot_empty
+                        assert(next_washtenaw(cur_spot, 2) == 0);
+                        next_washtenaw(cur_spot, 2) = right_car;
+                        % TODO: shift queue
+                    end
+                end
+            % turn logic for red light    
+            else
+                % right turn queue
+                right_car = queue_table(right_queue, 2);
+                if right_car > 0
+                    spot_empty = (washtenaw(cur_spot, 2) == 0 && washtenaw(cur_spot-1, 2) == 0);
+                    if spot_empty
+                        assert(next_washtenaw(cur_spot, 2) == 0);
+                        next_washtenaw(cur_spot, 2) = right_car;
+                        % TODO: shift queue
+                    end
+                end
+            end
+        end
+    end
 
 end
